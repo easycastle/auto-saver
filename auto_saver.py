a4b636318fa17c0f804a9184e228eba1d83b821e
sean3 <cocoquiet@knu.ac.kr>
2023-01-29 16:32:05 +0900

    feat : Add set_page

--- auto_saver.py
+++ auto_saver.py
@@ -1,5 +1,6 @@
 import pystray
 import PIL.Image
+from plyer import notification
 
 import pyautogui as pag
 import keyboard
@@ -8,14 +9,39 @@ import threading
 
 key = ['ctrl', 's']
 interval = 4
+page = None
 saveThread = None
+ableNotification = True
+
+
+def set_page(icon, item):
+    global page
+
+    pag.hotkey('alt', 'tab')
+    page = pag.getActiveWindow().title.split(' - ')[-1]
+
+    notification.notify(
+        app_name='Auto Saver',
+        title='Page set to',
+        message=page,
+        app_icon='./images/icon.ico',
+        timeout=5
+    )
 
 def save():
     global key, saveThread
+    
+    if page == None:
+        pag.alert('Please set page first', 'Error')
+        return
 
-    pag.hotkey(*key)
-    saveThread = threading.Timer(interval, save)
-    saveThread.start()
+    else:
+        if page != pag.getActiveWindow().title.split(' - ')[-1]:
+            return
+
+        pag.hotkey(*key)
+        saveThread = threading.Timer(interval, save)
+        saveThread.start()
 
 def on_clcik_start(icon, item):
     save()
@@ -43,6 +69,23 @@ def on_click_change_interval(icon, item):
         else:
             break
 
+# def on_click_enable_disable_notification(icon, item):
+#     global ableNotification
+
+#     if ableNotification:
+#         ableNotification = False
+#     else:
+#         ableNotification = True
+#         # notification.notify(
+#         #     app_name='Auto Saver',
+#         #     title='Notification',
+#         #     message='Notification is enabled',
+#         #     app_icon='./images/icon.ico',
+#         #     timeout=3
+#         # )
+
+#     print(ableNotification)
+
 def on_clcik_stop(icon, item):
     global saveThread
 
@@ -57,9 +100,11 @@ def on_click_exit(icon, item):
 icon = PIL.Image.open('./images/icon.ico')
 
 tray = pystray.Icon(name='Auto Saver', icon=icon, title='Auto Saver', menu=pystray.Menu(
+    pystray.MenuItem('Set Page', set_page),
     pystray.MenuItem('Start', on_clcik_start),
     pystray.MenuItem('Change Key', on_click_change_key),
     pystray.MenuItem('Change Interval', on_click_change_interval),
+    # pystray.MenuItem('Enable Notification', on_click_enable_disable_notification, default=not ableNotification, checked=lambda item: not ableNotification, radio=True, visible=True),
     pystray.MenuItem('Stop', on_clcik_stop),
     pystray.MenuItem('Exit', on_click_exit),
 ))
